a53362a995e0856cf3cbac1b88c4624974182b29
Loalcoon <Loalcoon@gmail.com>
2024-11-11 11:21:21 +0200

    First commin: Rock Paper Scissors game

---
+++ Scissors.py
@@ -0,0 +1,46 @@
+import random
+
+user = input("Rock/Paper/Scissors or Q to Exit: ")
+
+Rock = 1
+Paper = 2
+Scissors = 3
+
+while user != 'Q':
+    pc = random.randint(1, 3)
+    if user == 'Rock':
+        if pc == 1:
+            print(f"Draw/Computer input: {pc}")
+            user = input("Rock/Paper/Scissors or Q to Exit: ")
+            continue
+        elif pc == 2:
+            print("Computer Win")
+            break
+        elif pc == 3:
+            print("User Win")
+            break
+    elif user == 'Paper':
+        if pc == 1:
+            print("User Win")
+            break
+        elif pc == 2:
+            print(f"Draw/Computer input: {pc}")
+            user = input("Rock/Paper/Scissors or Q to Exit: ")
+            continue
+        elif pc == 3:
+            print("Computer Win")
+            break
+    elif user == 'Scissors':
+        if pc == 1:
+            print("Computer Win")
+            break
+        elif pc == 2:
+            print("User Win")
+            break
+        elif pc == 3:
+            print(f"Draw/Computer input: {pc}")
+            user = input("Rock/Paper/Scissors or Q to Exit: ")
+            continue
+    elif user != 'Rock' or user != 'Paper' or user != 'Scissors':
+        print('Wrong Input')
+        break
